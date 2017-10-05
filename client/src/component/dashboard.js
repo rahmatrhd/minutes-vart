d30cf9e0c2a46d25adba9d237b95b137391124e2
Hacktiv8 <hacktiv8@Hacktiv8s-Mac-mini-2.local>
2017-10-05 11:40:05 +0700

    tambah logo

--- client/src/component/dashboard.js
+++ client/src/component/dashboard.js
@@ -3,16 +3,24 @@ import {Layout} from 'antd'
 
 import './App.css'
 
-
-const { Content, Sider } = Layout
+const {Content, Sider} = Layout
 
 class Dashboard extends Component {
   render() {
     return (
       <Layout>
-        <Layout className= 'app'>
-          <Content className = 'kanban'>main content</Content>
-          <Sider className = 'discussion'>left sidebar</Sider>
+        <Layout className='App'>
+          <div className='kanban'>
+            <div className='logo'>
+              <img alt='logo' src='https://www.freelogoservices.com/api/main/images/1j+ojVVCOMkX9Wyrexe4hGfU16jF6EIMjkLP2ig3M2RE9gxvkSYrhfNi47hlc1xFtFwKhhQIc8E6iyd9'/>
+            </div>
+            <div className='content'>
+              DISINI ISI KANBAN
+            </div>
+          </div>
+          <div className='discussion'>
+            
+          </div>
         </Layout>
       </Layout>
     )
