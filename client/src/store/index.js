bb2f9052f7a3adfa3ae0e2a766acef6d3518b28f
pdvega <pdvegaa@gmail.com>
2017-10-05 18:10:35 +0700

    store done

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,19 @@
-import {createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import todoReducers from '../reducers/todo'
+import { reactReduxFirebase } from 'react-redux-firebase'
+import rootReducer from '../reducers/rootReducer'
 
-const store = createStore(todoReducers, applyMiddleware(thunk))
+const firebaseConfig = {
+  apiKey: "AIzaSyDQiAP5PfSYOaPItrqLJRU50m4k3asw1Ys",
+  authDomain: "minutes-vart.firebaseapp.com",
+  databaseURL: "https://minutes-vart.firebaseio.com",
+  projectId: "minutes-vart",
+  storageBucket: "minutes-vart.appspot.com",
+  messagingSenderId: "987319535932"
+}
+
+
+const middleware = applyMiddleware(thunk)
+const store = compose(reactReduxFirebase(rootReducers, middleware),)(createStore)
+
 export default store
