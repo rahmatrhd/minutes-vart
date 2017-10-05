c65d7a52246ce03ad6cf3204da90e8d6f65aec0b
Hacktiv8 <hacktiv8@Hacktiv8s-Mac-mini-2.local>
2017-10-05 13:54:36 +0700

    done dashboard layout

--- client/src/component/dashboard.js
+++ client/src/component/dashboard.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Layout} from 'antd'
+import {Layout, Collapse, Card, Avatar, Button} from 'antd'
 
 import './App.css'
 
 const {Content, Sider} = Layout
+const Panel = Collapse.Panel
 
 class Dashboard extends Component {
   render() {
@@ -12,19 +13,67 @@ class Dashboard extends Component {
         <Layout className='App'>
           <div className='kanban'>
             <div className='logo'>
-              <img alt='logo' src='https://www.freelogoservices.com/api/main/images/1j+ojVVCOMkX9Wyrexe4hGfU16jF6EIMjkLP2ig3M2RE9gxvkSYrhfNi47hlc1xFtFwKhhQIc8E6iyd9'/>
+              <div className='minutes'>
+                <img
+                  alt='logo'
+                  src='https://www.freelogoservices.com/api/main/images/1j+ojVVCOMkX9Wyrexe4hGfU16jF6EIMjkLP2ig3M2RE9gxvkSYrhfNi47hlc1xFtFwKhhQIc8E6iyd9'/>
+              </div>
+              <div className='name'>
+                <h1>Project Name</h1>
+              </div>
             </div>
             <div className='content'>
               DISINI ISI KANBAN
             </div>
           </div>
           <div className='discussion'>
-            
+            <div className='info'>
+              <div className='userinfo'>
+                <Avatar size="large" icon="user"/><br/>
+                <b>
+                  USERNAME
+                </b>
+              </div>
+              <div className='logout'>
+                <Button type="danger">Logout</Button>
+              </div>
+            </div>
+            <div className='active'>
+              <Card
+                title="Room title"
+                extra={< a href = "#" > Join < /a>}
+                style={{
+                marginBottom: '10px'
+              }}>
+                <p>Discussion topic</p>
+              </Card>
+            </div>
+            <div className='history'>
+              <Collapse bordered={false} className='collapse'>
+                <Panel header="This is panel header 1" key="1" style={customPanelStyle}>
+                  <p>blah</p>
+                </Panel>
+                <Panel header="This is panel header 2" key="2" style={customPanelStyle}>
+                  <p>blah</p>
+                </Panel>
+                <Panel header="This is panel header 3" key="3" style={customPanelStyle}>
+                  <p>blah</p>
+                </Panel>
+              </Collapse>
+            </div>
           </div>
         </Layout>
       </Layout>
     )
   }
 }
+
+const customPanelStyle = {
+  background: 'white',
+  borderRadius: 4,
+  marginBottom: 24,
+  border: 0,
+  overflow: 'hidden'
+};
 
 export default Dashboard
