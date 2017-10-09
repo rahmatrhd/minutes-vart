e6ad8163468f8023be3c033f0c5a12fc825ed709
ahmad-fajar <shin.fajar@gmail.com>
2017-10-09 12:17:06 +0700

    otorisasi edit kanban, delete, add

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
+import {  applyMiddleware, combineReducers, createStore } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducer from '../reducers/rootReducer'
 
+import userReducer from '../reducers/userReducer'
+
+const rootReducer = combineReducers({
+  userStore: userReducer
+})
 
 const middleware = applyMiddleware(thunk)
 const store = createStore(rootReducer, middleware)
